01109ad1e02f9c07006b54e21cee6973223bbb89
bonsai <onsen.bonsai@gmail.com>
2026-09-10 11:20:01 +0900

    refactor: open GitHub directly from side panel

--- sidepanel.js
+++ sidepanel.js
@@ -1,3 +1,3 @@
 document.getElementById("openGithub").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ type: "open-github-new" });
+  chrome.tabs.create({ url: "https://github.com/new" });
 });
